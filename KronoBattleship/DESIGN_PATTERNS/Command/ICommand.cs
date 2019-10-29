e65287b057506482c212964e375e3f83c0e161e3
Vilius Kerutis <kerutisvilius@gmail.com>
2019-10-29 15:50:35 +0200

    Command is done

---
+++ KronoBattleship/DESIGN_PATTERNS/Command/ICommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KronoBattleship.DESIGN_PATTERNS.Command
+{
+    interface ICommand
+    {
+        List<int> Execute(bool isHorizontal, int iteration);
+        List<int> Undo();
+    }
+}
