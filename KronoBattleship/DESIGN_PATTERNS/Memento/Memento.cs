754da75654a63733798fe0bf827a3025e9b42da3
auscep1 <ausra.cepulionyte@gmail.com>
2019-11-24 14:17:39 +0200

    memento

---
+++ KronoBattleship/DESIGN_PATTERNS/Memento/Memento.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KronoBattleship.DESIGN_PATTERNS.Memento
+{
+    //https://www.dofactory.com/net/memento-design-pattern
+
+    public class Memento
+    {
+        private string state;
+
+        /// <summary>
+        /// Memento constructor
+        /// </summary>
+        /// <param name="state"></param>
+        public Memento(string state)
+        {
+            this.state = state;
+        }
+
+        // Gets or sets state
+        public string State
+        {
+            get {
+                return state;
+            }
+        }
+    }
+
+    /// <summary>
+    /// The Caretaker class- is responsible for the memento's safekeeping 
+    /// </summary>
+    public class Caretaker
+    {
+        private Memento memento;
+
+        /// <summary>
+        /// Gets or sets memento
+        /// </summary>
+        public Memento Memento
+        {
+            set
+            {
+                memento = value;
+            }
+            get
+            {
+                return memento;
+            }
+        }
+    }
+
+    /// <summary>
+    /// The Originator class -creates a memento containing a snapshot of its current internal state
+    /// </summary>
+    class Originator
+    {
+        private string state;
+
+        /// <summary>
+        /// State property
+        /// </summary>
+        public string State
+        {
+            get { return state; }
+            set
+            {
+                state = value;
+                System.Diagnostics.Debug.WriteLine("State = " + state);
+            }
+        }
+
+        /// <summary>
+        /// Creates memento
+        /// </summary>
+        /// <returns></returns>
+        public Memento CreateMemento()
+        {
+            return (new Memento(state));
+        }
+
+        /// <summary>
+        /// Restores original state
+        /// </summary>
+        /// <param name="memento">Memento obj</param>
+        public void SetMemento(Memento memento)
+        {
+            System.Diagnostics.Debug.WriteLine("Restoring state...");
+            State = memento.State;
+        }
+    }
+}
