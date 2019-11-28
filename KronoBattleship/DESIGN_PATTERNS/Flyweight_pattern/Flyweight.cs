e57fae381e599bd744bddc991b997dbf4416b79c
NeringaLaimutyte <neringa.laimutyte@ktu.edu>
2019-11-28 22:49:05 +0200

    Flyweight (unused and without memory measurement)

---
+++ KronoBattleship/DESIGN_PATTERNS/Flyweight_pattern/Flyweight.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using KronoBattleship.DESIGN_PATTERNS.State;
+
+namespace KronoBattleship.DESIGN_PATTERNS.Flyweight_pattern
+{
+    class FlyweightFactory
+    {
+        private Dictionary<char, State.State> flyweights = new Dictionary<char, State.State>();
+        public State.State GetFlyweight(char key)
+        {
+            State.State flyweight;
+            if (flyweights.ContainsKey(key))
+            {
+                flyweight = flyweights[key];
+            }
+            else
+            {
+                switch (key)
+                {
+                    case 'C':
+                        flyweight = new ConcreteStateConnected();
+                        break;
+                    case 'W':
+                        flyweight = new Waiting();
+                        break;
+                    case 'P':
+                        flyweight = new Playing();
+                        break;
+                    default:
+                        return null;
+                }
+                flyweights.Add(key, flyweight);
+            }
+            return flyweight;
+        }
+    }
+}
