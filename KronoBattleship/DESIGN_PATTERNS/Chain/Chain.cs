eb79e78d02bae35cf751aaa9b470003c2f9c9f52
Vilius Kerutis <kerutisvilius@gmail.com>
2019-12-01 20:17:18 +0200

    Small changes in Chain.

--- KronoBattleship/DESIGN_PATTERNS/Chain/Chain.cs
+++ KronoBattleship/DESIGN_PATTERNS/Chain/Chain.cs
@@ -1,9 +1,4 @@
 ﻿using KronoBattleship.DESIGN_PATTERNS.Builder;
-using KronoBattleship.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace KronoBattleship.DESIGN_PATTERNS.Chain
 {
